f4f607863ea722fc95e1512b91b5ee5c0d235486
satriamaulana <55432210+satriamaulana@users.noreply.github.com>
2019-11-26 15:38:13 +0700

    fasilitas

---
+++ Design/fasilitas/detail_kos.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>PAPI KOST</title>
+</head>
+<style>
+    .navbar-nav .nav-item {
+        margin-left: 28px !important;
+    }
+</style>
+
+<body>
+    <!--bagian atas -->
+    <section class="atas">
+        <div class="container"></div>
+        <div calss="row">
+            <div calss="col-md-6"></div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a href="" class="navbar-brand">
+                    <img src="Tugas PPL.png" style="width: 60px;" />
+                </a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Tentang <span class="sr-only">(current)</span></a>
+                        </li>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <li class="nav-item active"></li>
+                        <form class="form-inline my-2 my-lg-0">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Masuk</button>
+                        </form>
+                        </li>
+                </div>
+            </nav>
+            <!--bagian produk -->
+            <section class="produk">
+                <h1 class="display-4">Kost AISYAH</h1>
+                <div class="card-group">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="tampak depan.jpg" style="width: 100%" />
+                                <button class="btn btn-primary">Simpan</button>
+                                <button class="btn btn-primary float-right">Delete</button>
+                            </div>
+                            <div class="col-md-4">
+                                <p>Lantai 1 (6 kamar)</p>
+                                <p>Lantai 2 (7 kamar)</p>
+                                <p>lantai 1 (Rp 400.000 - Rp 800.000</p>
+                                <p>lantai 1 (Rp 400.000)</p>
+                                <p>Ranting xxx</p>
+                                <button class="btn btn-primary">Update</button>
+                            </div>
+                            <div class="col-md-4">
+                                <p>Fasilitas Yang Tersedia</p>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <img src="dapur.png" style="width: 15%" />
+                                        <p>Dapur Bersama</p>
+                                        <img src="kamar mandi.png" style="width: 15%" />
+                                        <p>kamar mandi bersama</p>
+                                    </div>
+                                    <div class="col-md-6">
+
+                                        <img src="meja bersama.png" style="width: 15%" />
+                                        <p>Meja Santai</p>
+                                        <img src="living room.png" style="width: 15%" />
+                                        <p>Ruang Tamu</p>
+                                    </div>
+                                    <div class="col-md-12">
+                                    <p>Fasilitas umum terdekat</p>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <img src="masjid.png" style="width: 15%" />
+                                                <p>Masjid</p>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <img src="pom bensin.png" style="width: 15%" />
+                                                <p>Pom bensin</p>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <img src="supermarket.png" style="width: 15%" />
+                                                <p>Supermarket</p>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <img src="kampus.png" style="width: 15%" />
+                                                <p>Kampus</p>
+                                            </div>
+                                        </div>
+                                    </div>
+<!--                                     
+                                        <div class="col-md-4">
+                                        <p>Fasilitas umum terdekat</p>
+                                        <div class="col-md-6">
+                                            <img src="dapur.png" style="width: 15%" />
+                                            <p>Dapur Bersama</p>
+                                            <img src="kamar mandi.png" style="width: 15%" />
+                                            <p>kamar mandi bersama</p>
+                                        </div>
+                                    </div> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
